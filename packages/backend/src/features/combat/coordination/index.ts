6bf4f4bcf0d2f02508fc40b96a02b070623aedb6
wits-notebook <zywzjsc@gmail.com>
2025-12-12 14:26:31 +0800

    搬運大量資料

--- packages/backend/src/features/combat/coordination/index.ts
+++
@@ -1,7 +0,0 @@
-// Public system classes
-export { TickActionSystem } from './TickActionSystem'
-// Specific target selector implementations
-export { FirstAliveSelector } from '../target-select-strategies/FirstAliveSelector'
-export { LowestHealthSelector } from '../target-select-strategies/LowestHealthSelector'
-export type { ITargetSelector } from '../target-select-strategies/target-selector'
-// Note: Do not export DamageFactory (internal tool)
