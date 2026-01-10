5bfdefc6eb9531f21b79778b96e8e53b13fbcba8
SSSRankHunter <zywzjsc@gmail.com>
2026-01-10 21:05:16 +0800

    移動檔案位置

--- packages/game-core/scripts/ps1/repomix.ps1
+++ packages/game-core/scripts/ps1/repomix.ps1
@@ -1,2 +1,4 @@
-repomix --remove-comments --include "**/*.ts" --ignore "data/**" --style plain --output output_app.txt ./src/application
-repomix --remove-comments --include "**/*.ts" --ignore "data/**" --style plain --output output_domain.txt ./src/domain
+cd "g:\Coding\app_harpy"
+repomix --remove-comments --include "**/*.ts" --ignore "data/**" --style plain --output out/output_app.txt ./packages/game-core/src/application
+repomix --remove-comments --include "**/*.ts" --ignore "data/**" --style plain --output out/output_domain.txt ./packages/game-core/src/domain
+repomix --remove-comments --include "**/*.ts" --ignore "data/**" --style plain --output out/output_api.txt ./packages/backend-nest/src
